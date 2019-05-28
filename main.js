bcdc5333173c156a2d2bc7dc364d20d67800d0ce
melialbu <44821933+MeliAlbu@users.noreply.github.com>
2019-05-28 16:33:27 -0300

    Practicando forms

---
+++ main.js
@@ -0,0 +1,21 @@
+// var form = document.querySelector("form");
+// var passwordCorrecto = ("gatitos123") ;
+
+
+
+// form.onsubmit = function (evento) {
+//     evento.preventDefault();
+//     var password = document.getElementById("pass")
+//     if (password.value === passwordCorrecto) {
+
+//     } else {
+//         password.classList.add("error")
+//     }
+// }
+
+var checkboxes = document.getElementById("check");
+var gato = checkboxes[0];
+var perro = checkboxes[1];
+
+console.log(gato.checked); //retorna true
+
